fe934147542a3c4395b3c6c20ff03bff557377e4
tdreilloc <32942740+tdreilloc@users.noreply.github.com>
2019-03-10 10:16:16 -0400

    Update assign2.py
    Made image "global" in browseImage(). Works on my end.

--- assign2.py
+++ assign2.py
@@ -6,10 +6,57 @@ from tkinter import filedialog
 import tkinter.messagebox
 
 # declare image path as a global variable
-image = ""
+#image = ""
+
+def histoEqualizer():
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    equ = cv2.equalizeHist(gray)
+
+    equ = PIL.Image.fromarray(equ)
+
+    equ = ImageTk.PhotoImage(equ)
+
+    newImg.configure(image=equ)
+
+    newImg.image = equ
+
+    editLabel.configure(text="Histogram Equalization")
+
+def gaussianMask():
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    #kernal = cv2.getGaussianKernel()
+
+    gauss = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    gauss = PIL.Image.fromarray(gauss)
+
+    gauss = ImageTk.PhotoImage(gauss)
+
+    newImg.configure(image=gauss)
+
+    newImg.image = gauss
+
+    editLabel.configure(text="Gaussian Mask")
+
+
+def medianFilter():
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    med = cv2.medianBlur(gray, 3)
+
+    med = PIL.Image.fromarray(med)
+
+    med = ImageTk.PhotoImage(med)
+
+    newImg.configure(image=med)
+
+    newImg.image = med
+
+    editLabel.configure(text="Median Filtering")
 
 
 def browseImage():
+    global image
     path = filedialog.askopenfilename()
     image = cv2.imread(path)
 
@@ -28,54 +75,11 @@ def browseImage():
 
     imgLabel.configure(text="Original")
 
-    def histoEqualizer():
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        equ = cv2.equalizeHist(gray)
-
-        equ = PIL.Image.fromarray(equ)
-
-        equ = ImageTk.PhotoImage(equ)
-
-        newImg.configure(image=equ)
-
-        newImg.image = equ
-
-        editLabel.configure(text="Histogram Equalization")
-
-    def gaussianMask():
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        #kernal = cv2.getGaussianKernel()
-
-        gauss = cv2.GaussianBlur(gray, (5, 5), 0)
-
-        gauss = PIL.Image.fromarray(gauss)
-
-        gauss = ImageTk.PhotoImage(gauss)
-
-        newImg.configure(image=gauss)
-
-        newImg.image = gauss
-
-        editLabel.configure(text="Gaussian Mask")
-
-    def medianFilter():
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        med = cv2.medianBlur(gray, 3)
-
-        med = PIL.Image.fromarray(med)
-
-        med = ImageTk.PhotoImage(med)
-
-        newImg.configure(image=med)
-
-        newImg.image = med
-
-        editLabel.configure(text="Median Filtering")
 
     histogram.configure(command=histoEqualizer)
     median.configure(command=medianFilter)
     gaussian.configure(command=gaussianMask)
+
 
 # opens a messgae box about the details of the program
 def aboutProgram():
@@ -104,6 +108,7 @@ root.config(menu=menu)
 fileMenu = Menu(menu)
 menu.add_cascade(label="File", menu=fileMenu)
 fileMenu.add_command(label="Browse Image...", command=browseImage)
+#fileMenu.add_command(label="Save as..")
 fileMenu.add_separator()
 fileMenu.add_command(label="Exit", command=quit)
 
@@ -138,11 +143,11 @@ orgImg.pack(side=LEFT, expand=YES, fill=BOTH)
 newImg = Label(mainFrame, bg='white')
 newImg.pack(expand=YES, fill=BOTH)
 
-histogram = Button(toolbar, text="Histogram Equalization", command=doNothing)
+histogram = Button(toolbar, text="Histogram Equalization", command=histoEqualizer)
 histogram.pack(side=LEFT, padx=2, pady=2)
-median = Button(toolbar, text="Median Filtering", command=doNothing)
+median = Button(toolbar, text="Median Filtering", command=medianFilter)
 median.pack(side=LEFT, padx=2, pady=2)
-gaussian = Button(toolbar, text="Gaussian Mask", command=doNothing)
+gaussian = Button(toolbar, text="Gaussian Mask", command=gaussianMask)
 gaussian.pack(side=LEFT, padx=2, pady=2)
 
 status = Label(root, text="Preparing to do nothing...", bd=1, relief=SUNKEN, anchor=W)
