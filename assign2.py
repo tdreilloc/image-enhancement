e881471d3873e3b5e2b4e5da9e0145c0ad2b4120
tdreilloc <32942740+tdreilloc@users.noreply.github.com>
2019-03-11 18:41:05 -0400

    Update assign2.py
    Put in two edge detectors. the Sobel edge detecting function is commented out because it wasn't working. Doesn't matter really since the Canny edge detection first smooths, then uses a Sobel mask to perform the edge detection. It works pretty well. The MRI one looks super cool if you:
    
    1. perform histogram equalizer
    2. median filter
    3. Canny edge detection

--- assign2.py
+++ assign2.py
@@ -4,78 +4,147 @@ import PIL.Image
 from tkinter import *
 from tkinter import filedialog
 import tkinter.messagebox
-
-# declare image path as a global variable
-image = ""
-
+from functools import partial
 
 def browseImage():
+    global image
+    global imageEdit
+    global path
+
     path = filedialog.askopenfilename()
-    image = cv2.imread(path)
+    imageEdit = cv2.imread(path)
+    image = imageEdit
 
     # OpenCV represents images in BGR order; however PIL represents
     # images in RGB order, so we need to swap the channels
     orig = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
     # convert the images to PIL format...
-    orig = PIL.Image.fromarray(orig)
+    origPIL = PIL.Image.fromarray(orig)
 
     # ...and then to ImageTk format
-    orig = ImageTk.PhotoImage(orig)
+    origTK = ImageTk.PhotoImage(origPIL)
 
-    orgImg.configure(image=orig)
-    orgImg.image = orig
+    orgImg.configure(image=origTK)
+    orgImg.image = origTK
 
     imgLabel.configure(text="Original")
 
-    def histoEqualizer():
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        equ = cv2.equalizeHist(gray)
+    image = orig
 
-        equ = PIL.Image.fromarray(equ)
+def saveImage(img):
+    global image
+    global path
+    cv2.imwrite(path+".jpg", img)
 
-        equ = ImageTk.PhotoImage(equ)
 
-        newImg.configure(image=equ)
+def originalImage():
+    global image
+    global imageEdit
 
-        newImg.image = equ
+    #Reset image to original
+    image = imageEdit
 
-        editLabel.configure(text="Histogram Equalization")
+    #Convert to make it viewable in Tkinter
+    imagePIL = PIL.Image.fromarray(imageEdit)
+    imageTK = ImageTk.PhotoImage(imagePIL)
 
-    def gaussianMask():
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #Label stuff
+    orgImg.configure(image=imageTK)
+    orgImg.image=imageTK
+    editLabel.configure(text="Original")
 
-        #kernal = cv2.getGaussianKernel()
+    #Show image
+    showNewImage(imageTK)
 
-        gauss = cv2.GaussianBlur(gray, (5, 5), 0)
+def histoEqualizer(img):
+    global image
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    equ = cv2.equalizeHist(gray)
 
-        gauss = PIL.Image.fromarray(gauss)
+    equPIL = PIL.Image.fromarray(equ)
 
-        gauss = ImageTk.PhotoImage(gauss)
+    equTk = ImageTk.PhotoImage(equPIL)
 
-        newImg.configure(image=gauss)
+    editLabel.configure(text="Histogram Equalization")
 
-        newImg.image = gauss
+    showNewImage(equTk)
 
-        editLabel.configure(text="Gaussian Mask")
+    #Changes global image to filtered image
+    image = equ
 
-    def medianFilter():
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        med = cv2.medianBlur(gray, 3)
 
-        med = PIL.Image.fromarray(med)
+def gaussianMask(img):
+    global image
+    #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        med = ImageTk.PhotoImage(med)
+    gauss = cv2.GaussianBlur(img, (3, 3), 0)
 
-        newImg.configure(image=med)
+    gaussPIL = PIL.Image.fromarray(gauss)
 
-        newImg.image = med
+    gaussTk = ImageTk.PhotoImage(gaussPIL)
 
-        editLabel.configure(text="Median Filtering")
+    editLabel.configure(text="Gaussian Mask")
 
-    histogram.configure(command=histoEqualizer)
-    median.configure(command=medianFilter)
-    gaussian.configure(command=gaussianMask)
+    showNewImage(gaussTk)
+
+    #Changes global image to filtered image
+    image = gauss
+
+def medianFilter(img):
+    global image
+    #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    med = cv2.medianBlur(img, 3)
+
+    medPIL = PIL.Image.fromarray(med)
+
+    medTk = ImageTk.PhotoImage(medPIL)
+
+    editLabel.configure(text="Median Filtering")
+
+    showNewImage(medTk)
+
+    #Changes global image to filtered image
+    image = med
+
+def edgeCanny(img):
+    global image
+
+    edge = cv2.Canny(img, 100, 200)
+
+    edgePIL = PIL.Image.fromarray(edge)
+
+    edgeTK = ImageTk.PhotoImage(edgePIL)
+
+    editLabel.configure(text="Edges (Canny)")
+
+    showNewImage(edgeTK)
+
+    image = edge
+
+#def edgeSobel(img):
+#    global image
+#
+#    edge = cv2.Sobel(img, 3, 3, 3)
+#
+#    edgePIL = PIL.Image.fromarray(edge)
+#
+#    edgeTK = ImageTk.PhotoImage(edgePIL)
+#
+#    editLabel.configure(text="Edges (Canny)")
+#
+#    showNewImage(edgeTK)
+#
+#    image = edge
+
+
+def showNewImage(img):
+    newImg.configure(image=img)
+
+    newImg.image = img
+
+def undo():
+    global image
 
 # opens a messgae box about the details of the program
 def aboutProgram():
@@ -84,12 +153,7 @@ def aboutProgram():
 
 # opens a messgae box on how to use the program
 def helpBox():
-    tkinter.messagebox.showinfo("How to Use", "To use this program blah blah blah")
-
-
-def doNothing():
-    print("nothing")
-
+    tkinter.messagebox.showinfo("1. Click Browse to choose image.2. Apply desired filters.3. Apply desired edge detection.4. Save image if desired.")
 
 # opens up a window, config title bar and size
 root = Tk()
@@ -104,6 +168,7 @@ root.config(menu=menu)
 fileMenu = Menu(menu)
 menu.add_cascade(label="File", menu=fileMenu)
 fileMenu.add_command(label="Browse Image...", command=browseImage)
+fileMenu.add_command(label="Save as..", command=lambda: saveImage(image))
 fileMenu.add_separator()
 fileMenu.add_command(label="Exit", command=quit)
 
@@ -113,39 +178,49 @@ menu.add_cascade(label="Help", menu=helpMenu)
 helpMenu.add_command(label="Help", command=aboutProgram)
 helpMenu.add_command(label="About", command=helpBox)
 
-# add a toolbar to the top of the window
-toolbar = Frame(root, bd=1, relief=SUNKEN)
-toolbar.pack(side=TOP, fill=X)
-# add a browse image button to the toolbar
-browse = Button(toolbar, text="Browse...", command=browseImage)
+#Add another toolbar for browsing/saving
+fileButtons = Frame(root, bd=1, relief=SUNKEN)
+fileButtons.pack(side=TOP, fill=X)
+
+#Add buttons to toolbar
+browse = Button(fileButtons, text="Browse...", command=browseImage)
 browse.pack(side=LEFT, padx=2, pady=2)
+save = Button(fileButtons, text="Save as...", command=lambda: saveImage(image))
+save.pack(side=LEFT, padx=2, pady=2)
+
+# add a filterButtons to the top of the window
+filterButtons = Frame(root, bd=1, relief=SUNKEN)
+filterButtons.pack(side=TOP, fill=X)
+#Add buttons to toolbar
+histogram = Button(filterButtons, text="Histogram Equalization", command=lambda: histoEqualizer(image)).pack(side=LEFT, padx=2, pady=2)
+median = Button(filterButtons, text="Median Filtering", command=lambda: medianFilter(image)).pack(side=LEFT, padx=2, pady=2)
+gaussian = Button(filterButtons, text="Gaussian Mask", command=lambda: gaussianMask(image)).pack(side=LEFT, padx=2, pady=2)
+original = Button(filterButtons, text="Original", command=lambda: originalImage()).pack(side=LEFT, padx=2, pady=2)
+
+#Add edge detection to a new toolbar
+edgeButtons = Frame(root, bd=1, relief=SUNKEN)
+edgeButtons.pack(side=TOP, fill=X)
+#Add buttons to toolbar
+canny = Button(edgeButtons, text="Canny Edges", command=lambda: edgeCanny(image)).pack(side=LEFT, padx=2, pady=2)
+sobel = Button(edgeButtons, text="Sobel Filter", command=lambda: edgeSobel(image)).pack(side=LEFT, padx=2, pady=2)
 
 # add a main frame
 mainFrame = Frame(root, width=700, height=700)
 mainFrame.pack()
 
-# title label
-welcomeLabel = Label(mainFrame, text="WELCOME", font="Helvetica", fg="purple")
-welcomeLabel.pack(side=TOP, pady=5)
 imgLabel = Label(mainFrame, text="", font="Helvetica", fg="black")
 imgLabel.pack(side=LEFT)
 editLabel = Label(mainFrame, text="", font="Helvetica", fg="black")
 editLabel.pack(side=RIGHT)
 
-
+#Display original image
 orgImg = Label(mainFrame, bg='white')
 orgImg.pack(side=LEFT, expand=YES, fill=BOTH)
+
+#Display after applying filter
 newImg = Label(mainFrame, bg='white')
 newImg.pack(expand=YES, fill=BOTH)
 
-histogram = Button(toolbar, text="Histogram Equalization", command=doNothing)
-histogram.pack(side=LEFT, padx=2, pady=2)
-median = Button(toolbar, text="Median Filtering", command=doNothing)
-median.pack(side=LEFT, padx=2, pady=2)
-gaussian = Button(toolbar, text="Gaussian Mask", command=doNothing)
-gaussian.pack(side=LEFT, padx=2, pady=2)
-
-status = Label(root, text="Preparing to do nothing...", bd=1, relief=SUNKEN, anchor=W)
-status.pack(side=BOTTOM, fill=X)
+Label(root, text="When finished, File --> Exit...", bd=1, relief=SUNKEN, anchor=W).pack(side=BOTTOM, fill=X)
 
 root.mainloop()
